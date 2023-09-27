e3414062fb8d2b566c6a1c71db1f65c4f30d8fef
IWhitebird <patangeshreyas@gmail.com>
2023-09-27 16:21:12 +0530

    Update workshop.tsx

--- src/pages/workshop.tsx
+++ src/pages/workshop.tsx
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux"
 import Navbar from "../components/Navbar"
 import { RootState } from "../reducer";
 import Card from "../components/Card";
-import { Link } from "react-router-dom";
 import { useEffect , useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
